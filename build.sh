c143df7201d637050b9e6ca866a48cb75aa80eea
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-08-24 21:10:36 -0700

    Move stuff into kv-store directory

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags -w hello.go
-docker build -t hello .
---
+++ kv-store/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CGO_ENABLED=0 GOOS=linux go build -o hello.exe -a -tags netgo -ldflags -w hello.go
+
+docker build -t hello .
